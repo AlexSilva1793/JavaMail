1eff85985bca6642872ff2ac978f5ca0d0000445
Alex Silva <easgaitan@gmail.com>
2019-12-03 20:52:39 -0500

    Configurar web.xml

--- src/main/java/com/alex/javamail/EnvioCorreo.java
+++ src/main/java/com/alex/javamail/EnvioCorreo.java
@@ -7,6 +7,7 @@ package com.alex.javamail;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,9 +21,25 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "EnvioCorreo", urlPatterns = {"/EnvioCorreo"})
 public class EnvioCorreo extends HttpServlet {
 
+    private String host, puerto, usuario, clave;
+
+    public void init() {
+        ServletContext context = getServletContext();
+        host = context.getInitParameter("host");
+        puerto = context.getInitParameter("puerto");
+        usuario = context.getInitParameter("usuario");
+        clave = context.getInitParameter("clave");
+    }
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        String destino = request.getParameter("textDestino");
+        String asunto = request.getParameter("textAsunto");
+        String mensaje = request.getParameter("textMensaje");
+        
+        String resultadoEnvio ="";
+        
     }
 
 }
